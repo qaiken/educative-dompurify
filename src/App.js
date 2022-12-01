65d5dfa50d71812a4b4d8b83a8ef759e554f9df7
Quinton Aiken <quintonlaiken@gmail.com>
2022-11-30 21:34:23 -0600

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import * as DOMPurify from 'dompurify';
+
 import './App.css';
 
 function App() {
+  const [message, setMessage] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input onChange={(e) => setMessage(e.target.value)} />
+      <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(message) }} />
     </div>
   );
 }
