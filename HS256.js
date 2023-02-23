f3a2be5396e8b7ddc561ce9de405b8be293e71a1
Quinton Aiken <quintonlaiken@gmail.com>
2023-02-22 21:53:12 -0800

    jsonwebtokens

---
+++ HS256.js
@@ -0,0 +1,8 @@
+// Signing (Sender)
+const jwt = require('jsonwebtoken');
+const payload = { foo: 'bar' };
+const token = jwt.sign(payload, 'my-cool-secret'); // HS256 by default
+
+// Verification (Receiver)
+const decoded = jwt.verify(token, 'my-cool-secret');
+console.log(decoded.foo); // bar
